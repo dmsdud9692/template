1b46c7865d949e17544308c94aead0070bcda8bb
dmsdud9692 <62579588+dmsdud9692@users.noreply.github.com>
2020-04-14 10:33:33 +0900

    Add files via upload
    템플릿 업로드

---
+++ template/js/menu.js
@@ -0,0 +1,10 @@
+str  = ''
+str += '<header class="header">'
+str += '    <ul>'
+str += '        <li><a href="01_font.html">FONT</a></li>'
+str += '        <li><a href="02_table.html">TABLE</a></li>'
+str += '    </ul>'
+str += '</header>'
+
+document.write( str );
+ 
